8fdc45b6cd8101775994609b7303b5ed6cac674e
alexandre minot <alexandre.minot@hotmail.fr>
2018-11-20 16:51:52 +1100

    condition report edit view

--- app/controllers/condition_reports_controller.rb
+++ app/controllers/condition_reports_controller.rb
@@ -2,37 +2,37 @@ class ConditionReportsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @cr = Cr.all
+    @conditionreport = ConditionReport.all
   end
 
   def show
-    @cr = Cr.find(params[:id])
+    @conditionreport = ConditionReport.find(params[:id])
   end
 
   def new
-    @cr = Cr.new
+    @conditionreport = ConditionReport.new
   end
 
   def create
-    @cr = Cr.new(cr_params)
-    @cr.user = current_user
-    @cr.save
+    @conditionreport = ConditionReport.new(conditionreport_params)
+    @conditionreport.user = current_user
+    @conditionreport.save
     redirect_to condition_report_path, notice: 'You are on the good way!'
   end
 
   def edit
-    @cr = Cr.find(params[:id])
+    @conditionreport = ConditionReport.find(params[:id])
   end
 
   def update
-    @cr = Cr.find(params[:id])
-    @cr.update(cr_params)
-    redirect_to condition_report_path(@cr)
+    @conditionreport = ConditionReport.find(params[:id])
+    @conditionreport.update(conditionreport_params)
+    redirect_to condition_report_path(@conditionreport)
   end
 
   private
 
-  def cr_params
-    params.require(:condition_reports).permit(:date_of_creation, :date_of_update)
+  def conditionreport_params
+    params.require(:condition_reports).permit(:owner_signed, :tenant_signed)
   end
 end
