b72139c949088c271bfe071998f0b12232073f82
Alix Palmer <alix.palmer@hotmail.com>
2018-11-20 16:50:22 +1100

    Additions to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,18 @@
 class CommentsController < ApplicationController
   def new
-    @room = Room.find(params[:room_element_id])
+    @room_element = RoomElement.find(params[:room_element_id])
     @comment = Comment.new
   end
 
   def create
+    @room_element = RoomElement.find(params[:room_element_id])
+    @comment = Comment.new(comment_params)
+    @room_element.comment = @comment
+  end
+
+  private
+
+  def comment_params
+    params.require(:commment).permit(:text_comment, :state, :photo)
   end
 end
