701ac2042c3ac20c40bdf8a3359525243292618c
tair tidhar <tairtidhar01@gmail.com>
2018-11-28 16:07:46 +1100

    made page stick to comment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,4 +2,5 @@ import "bootstrap";
 import "./components/hide_comments.js"
 import "./components/card_expand.js"
 import "./components/camera_button.js"
+import "./components/comment-button.js"
 import "./components/agree_button_submit.js"
